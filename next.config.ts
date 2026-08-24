7ff436ef577abd8b246e8b32011e1b05a29f9e9c
Suliman Shah <your_github_email@example.com>
2026-08-24 22:22:25 +0430

    commit for ios deep linking 6

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,16 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        // Intercepts the public static path
         source: '/.well-known/apple-app-site-association',
         headers: [
           {
             key: 'Content-Type',
             value: 'application/json',
+          },
+          {
+            key: 'Content-Disposition',
+            value: 'inline', // Prevents the browser from forcing a local download
           },
         ],
       },
